e02dc02b39bd3f6dcb13955c50213ec5b5546b27
AaribAzfar <aaribazfar3@gmail.com>
2026-04-27 23:53:09 +0930

    Added pakage installation file and changes to io

--- numcompute/io.py
+++ numcompute/io.py
@@ -1,8 +1,10 @@
 import numpy as np
 
 #reads a CSV file and converts it into a numpy array
-def read_csv(file_path, delimiter=',', dtype=float,missing_val=''):
+def read_csv(file_path, delimiter=',', dtype=object,missing_val=''):
     
     #using genfromtxt so it can handle missing values
-    return np.genfromtxt(file_path, delimiter=delimiter,dtype=dtype, missing_values=missing_val, filling_values=np.nan)
-
+    arr = np.genfromtxt(file_path, delimiter=delimiter,dtype=dtype, missing_values=missing_val, filling_values=np.nan,encoding="utf-8")
+    return np.vectorize(
+        lambda x: x.decode('utf-8') if isinstance(x, bytes) else x
+    )(arr)
